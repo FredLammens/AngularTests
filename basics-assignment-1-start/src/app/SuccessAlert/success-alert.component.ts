205d953e6c6fcb368a9ccb727ddaf5be5c7d7eef
Biebem <lammens.f96@gmail.com>
2021-01-28 13:56:08 +0100

    init

---
+++ basics-assignment-1-start/src/app/SuccessAlert/success-alert.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"succes-alert",
+    templateUrl:"./succes-alert.component.html",
+    styles:[`p {
+        color:dodgerblue;
+    }`]
+}
+)
+export class SuccesAlertComponent{
+
+}

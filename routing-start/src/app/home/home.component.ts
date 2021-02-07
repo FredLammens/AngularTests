cd766ccfa98ee407c2aa2f32a5400dfca1691aba
Biebem <lammens.f96@gmail.com>
2021-02-07 19:29:43 +0100

    added some routing exce

---
+++ routing-start/src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent{
+
+  constructor(private router:Router) { }
+  onLoadServers(id:number){
+    //complex calculation
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
+  }
+}
---
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ServersService } from '../servers.service';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css']
+})
+export class EditServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+  serverName = '';
+  serverStatus = '';
+
+  constructor(private serversService: ServersService, private route:ActivatedRoute) {
+
+   }
+
+  ngOnInit() {
+    console.log(this.route.snapshot.queryParams); //only run at creation of component
+    console.log(this.route.snapshot.fragment); 
+    this.route.queryParams.subscribe(); //to react to changes of component
+    this.route.fragment.subscribe(); //also auto unsubscribe
+    this.server = this.serversService.getServer(1);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+  }
+
+}

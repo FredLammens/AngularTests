29ba54c4bffc06863c8857c4d9a5839708ff3f82
Biebem <lammens.f96@gmail.com>
2021-02-07 16:28:09 +0100

    added some extra services

--- recipeShopProject/src/app/recipes/recipe.service.ts
+++ recipeShopProject/src/app/recipes/recipe.service.ts
@@ -1,11 +1,13 @@
 import { EventEmitter, Injectable, Output } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService{
     //---------------------------------------------------------------------------------------------------------------------------------------
     @Output() recipeSelected = new EventEmitter<Recipe>();
+    constructor(private slService:ShoppingListService) {}
     //---------------------------------------------------------------------------------------------------------------------------------------
     private recipes: Recipe[] = [
         new Recipe("BIO nutella!","nut based spread","https://static.openfoodfacts.org/images/products/356/007/047/2888/front_fr.69.full.jpg", [new Ingredient("hazelnut",5), new Ingredient("cashew",10)]),
@@ -18,5 +20,8 @@ export class RecipeService{
     ];
       getRecipes(){
           return this.recipes.slice(); //returns new array that is copy of 
+    }
+    addIngredientToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
     }
     }

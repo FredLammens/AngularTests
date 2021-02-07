68a922ad820f2a05ca7c12fdc013e51ba212898a
Biebem <lammens.f96@gmail.com>
2021-02-07 21:38:05 +0100

    added some routing exe

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService, private route:ActivatedRoute) {
 
@@ -20,7 +21,11 @@ export class EditServerComponent implements OnInit {
   ngOnInit() {
     console.log(this.route.snapshot.queryParams); //only run at creation of component
     console.log(this.route.snapshot.fragment); 
-    this.route.queryParams.subscribe(); //to react to changes of component
+    this.route.queryParams.subscribe(
+      (queryParams:Params) => {
+        this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+      }
+    ); //to react to changes of component
     this.route.fragment.subscribe(); //also auto unsubscribe
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;

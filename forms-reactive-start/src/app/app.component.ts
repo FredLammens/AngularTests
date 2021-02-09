8c1ffaae4be81db87f96f70e6b43fc5f03f24784
Biebem <lammens.f96@gmail.com>
2021-02-09 15:46:09 +0100

    finished reactive forms exe

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,14 +17,25 @@ export class AppComponent implements OnInit{
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username':  new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email' : new FormControl(null, [Validators.required,Validators.email])
+        'email' : new FormControl(null, [Validators.required,Validators.email],[this.forbiddenEmails])
       }),
       'gender' : new FormControl('male') ,
       'hobbies' : new FormArray([])
     }); 
+    this.signupForm.statusChanges.subscribe(
+      (value) => console.log(value)
+      ); //statusChanges => returns valid/pending/invalid  |  valueschanges => returns form object 
+      this.signupForm.patchValue(
+        {
+          'userData': {
+            'username': 'Fre'
+          }
+        }
+      );
   }
   onSubmit(){
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
   onAddHobby(){
     const control = new FormControl(null, Validators.required);
@@ -36,5 +48,16 @@ export class AppComponent implements OnInit{
     if(this.forbiddenUsernames.indexOf(control.value) !== -1) //javascript this is not from this obejct but from form control so error!
     return {'nameIsForbidden': true}; //errorcode = true
     return null; //if validation is succesfull
+  }
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any>{ //also bind this if u use it
+    const promise = new Promise<any>((resolve,reject) => {
+      setTimeout(() => {
+        if(control.value === 'test@test.com')
+        resolve({'emailIsForbidden': true});
+        else
+        resolve(null);12
+      },1500);
+    });
+    return promise;
   }
 }

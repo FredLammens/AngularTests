489f194f56453f0d735aab7dba28e91646989579
Biebem <lammens.f96@gmail.com>
2021-02-16 17:28:27 +0100

    split project into modules

--- recipeShopProject/src/app/app-routing.module.ts
+++ recipeShopProject/src/app/app-routing.module.ts
@@ -1,27 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthComponent } from "./auth/auth.component";
-import { AuthGuard } from "./auth/auth.gaurd";
 import { NotFoundComponent } from "./not-found/not-found.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
-import { RecipeSelectComponent } from "./recipes/recipe-select/recipe-select.component";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { RecipesResolverService } from "./recipes/recipes.resolver.service";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 const appRoutes: Routes = [
     {path: '',redirectTo: '/recipes', pathMatch: 'full'},
-    {path: 'recipes', component:RecipesComponent, canActivate: [AuthGuard], children: [
-        {path: '',component:RecipeSelectComponent},
-        {path: 'new',component:RecipeEditComponent},
-        {path: ':id',component:RecipeDetailComponent, resolve: [RecipesResolverService]},
-        {path: ':id/edit',component:RecipeEditComponent,  resolve: [RecipesResolverService]}
-    ]},
-    {path: 'shopping-list',component:ShoppingListComponent},
-    {path: 'auth', component: AuthComponent},
-    {path: 'not-found', component:NotFoundComponent},
-    {path: '**', redirectTo:'/not-found' }
+    {path: 'auth', component: AuthComponent}
+    //{path: 'not-found', component:NotFoundComponent},
+    //{path: '**', redirectTo:'/not-found' }
 ]
 
 @NgModule({
---
+++ recipeShopProject/src/app/recipes/recipes.module.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeSelectComponent } from "./recipe-select/recipe-select.component";
+import { RecipesRoutingModule } from "./recipes-routing.module";
+import { RecipesComponent } from "./recipes.component";
+
+@NgModule({
+    declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeSelectComponent,
+    RecipeEditComponent
+    ],
+    imports: [RouterModule, CommonModule ,ReactiveFormsModule, RecipesRoutingModule], //services doesnt need to be important all others do. => Commonmodule = for ngif etc.
+})
+export class RecipesModule {
+
+}
